23c0f8c709cccef371c2f03fd01578116cb82dbe
Rodriguex201 <emanuelgr150@gmail.com>
2026-05-15 16:15:51 -0500

    Cambio de /proformas bueno

--- app/Http/Controllers/ProformasController.php
+++ app/Http/Controllers/ProformasController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\View\View;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
